647273ddcc4bf90ef6dd46fb9935c74bd97f6086
unknown <jordipalacioseso@gmail.com>
2023-09-06 20:23:08 +0200

    Header Done V2

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,16 @@
 import './header.css';
-
 import { ReactComponent as Logo } from '../img/logo.svg';
 import { ReactComponent as Profile } from '../img/profile.svg';
 
 export function Header () {
     return (
         <div className= "header">
-            <div className="logo" >
-                <Logo />
-                <a href="/">Metas App</a>
+            <div className="container" >
+                <Logo className="logo"/>
+                <a href="/" className='tittle'>Metas App</a>
             </div>
             <nav>
-                <a href="/perfil" className="logo">
+                <a href="/profile" className="vincle">
                     <Profile className="icon"/>
                 </a>
             </nav>
