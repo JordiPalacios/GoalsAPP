61b3641b9b27215b1feb5059100487687fb001de
unknown <jordipalacioseso@gmail.com>
2023-09-06 20:12:20 +0200

    Headaer Done

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import { Main } from './components/main.js';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
