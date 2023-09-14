091fa1b57f5bed8fb9ae1766e847c8e4c66af14b
unknown <jordipalacioseso@gmail.com>
2023-09-14 21:03:04 +0200

    startStandardList

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,42 +1,11 @@
+import { useContext } from 'react';
 import Goal from './goal.js'
-
-const listMock = [
-    {
-        "id":"1",
-        "details":"Practice 30' Frontend Coding",
-        "period":"day",
-        "events":1,
-        "icon":"👨‍💻",
-        "goal":161,
-        "deadline":"2024-01-01",
-        "completed":1
-    },
-    {
-        "id":"2",
-        "details":"Listen 10' Entrepreneur Podcasts",
-        "period":"day",
-        "events":1,
-        "icon":"🎧",
-        "goal":161,
-        "deadline":"2024-01-01",
-        "completed":1
-    },
-    {
-        "id":"3",
-        "details":"Upload Drone Reels",
-        "period":"month",
-        "events":10,
-        "icon":"🚀",
-        "goal":40,
-        "deadline":"2024-01-01",
-        "completed":0
-    }
-
-];
+import { Context } from '../../services/memory.js';
 
 function List() {
+    const goals = useContext(Context);
     return (  
-       listMock.map(meta => <Goal {...meta}></Goal>)
+       goals.map(goal => <Goal key={goal.id} {...goal}></Goal>)
     );
 }
 
--- src/services/memory.js
+++ src/services/memory.js
@@ -1,10 +1,50 @@
-import { Children, createContext } from "react";
+import { createContext, useReducer } from "react";
+
+const listMock = [
+    {
+        "id":"1",
+        "details":"Practice 30' Frontend Coding",
+        "period":"day",
+        "events":1,
+        "icon":"👨‍💻",
+        "goal":161,
+        "deadline":"2024-01-01",
+        "completed":1
+    },
+    {
+        "id":"2",
+        "details":"Listen 10' Entrepreneur Podcasts",
+        "period":"day",
+        "events":1,
+        "icon":"🎧",
+        "goal":161,
+        "deadline":"2024-01-01",
+        "completed":1
+    },
+    {
+        "id":"3",
+        "details":"Upload Drone Reels",
+        "period":"month",
+        "events":10,
+        "icon":"🚀",
+        "goal":40,
+        "deadline":"2024-01-01",
+        "completed":0
+    }
+
+];
+
+const initState = {
+    order: [],
+    objects: {}
+};
 
 export const Context = createContext(null);
 
 function Memory( {children} ) {
+    const [state, dispatch] = useReducer(reducer, initState);
     return ( 
-        <Context.Provider value={'shared'}>
+        <Context.Provider value={[state, dispatch]}>
             {children}
         </Context.Provider>
      );
